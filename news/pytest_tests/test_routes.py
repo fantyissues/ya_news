07706572680cbe00a200a24d3cd62ccfff6e8374
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-18 21:05:40 +0500

    Добавить оставшиеся тесты маршрутов

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -19,3 +19,32 @@ from pytest_django.asserts import assertRedirects
 def test_pages_availability(client, name, args):
     response = client.get(reverse(name, args=args))
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK),
+        (pytest.lazy_fixture('reader_client'), HTTPStatus.NOT_FOUND),
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('news:edit', 'news:delete'),
+)
+def test_availability_for_comment_edit_and_delete(
+    parametrized_client, expected_status, name, comment
+):
+    response = parametrized_client.get(reverse(name, args=(comment.pk,)))
+    assert response.status_code == expected_status
+
+@pytest.mark.parametrize(
+    'name',
+    ('news:edit', 'news:delete'),
+)
+def test_redirect_for_anonymous_client(name, client, comment):
+    login_url = reverse('users:login')
+    url = reverse(name, args=(comment.pk,))
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
